1f2ac6c19aeca11afa3de5ce1c69597f502ae6b2
navneedhdev <navneedhdev>
2017-03-12 10:21:37 +1100

    committed
    tests the Facebook login functionality

---
+++ src/test/java/maven/LoginTest.java
@@ -0,0 +1,20 @@
+package maven;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.Test;
+
+public final class LoginTest {
+
+	@Test	
+	public void verifyFacebookLogin(){
+	System.setProperty("phantomjs.binary.path", "/Users/Dev/Downloads/phantomjs-2.1.1-macosx/bin/phantomjs");	
+	DesiredCapabilities cap = DesiredCapabilities.phantomjs();	
+	WebDriver driver = new PhantomJSDriver(cap);
+	driver.get("https://www.facebook.com");
+	System.out.println(driver.getTitle());
+		
+	}
+	
+}

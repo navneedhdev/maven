9ea570855bb38636a113b19d2ac65f46e8ae3601
navneedhdev <navneedhdev>
2017-03-12 21:44:59 +1100

    commit
    committed

--- src/test/java/maven/LoginTest.java
+++ src/test/java/maven/LoginTest.java
@@ -7,23 +7,42 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.concurrent.TimeUnit;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxProfile;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 
 public final class LoginTest {
 
 	@Test	
-	public void verifyFacebookLogin(){
+	public void verifyFacebookLogin() throws IOException{
 	System.setProperty("phantomjs.binary.path", "/usr/local/bin/phantomjs");	
 	DesiredCapabilities cap = DesiredCapabilities.phantomjs();	
 	WebDriver driver = new PhantomJSDriver(cap);
 	driver.get("https://www.facebook.com");
+	 TakesScreenshot scrShot =((TakesScreenshot)driver);
+
+     //Call getScreenshotAs method to create image file
+
+             File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
+
+         //Move image file to new destination
+
+             File DestFile=new File("/Users/Dev/Downloads/screenshot1.jpg");
+
+             //Copy file at destination
+
+             FileUtils.copyFile(SrcFile, DestFile);
 	System.out.println(driver.getTitle());
 	driver.quit();
 	}
@@ -31,20 +50,38 @@ public final class LoginTest {
 	@Test
 	public void mainTest() throws InterruptedException, AWTException, IOException
 	{
-		System.setProperty("webdriver.gecko.driver","/usr/local/bin/geckodriver");
-	/*	FirefoxProfile profile = new FirefoxProfile(new File("/Users/Dev/Library/Application Support/Firefox/Profiles/j3y30yg4.dev-edition-default"));                  
-		WebDriver driver = new FirefoxDriver(profile);*/
-	/*FirefoxProfile fxProfile = new FirefoxProfile();
+		/*System.setProperty("webdriver.gecko.driver","/usr/local/bin/geckodriver");
+		FirefoxProfile profile = new FirefoxProfile(new File("/Users/Dev/Library/Application Support/Firefox/Profiles/j3y30yg4.dev-edition-default"));                  
+		WebDriver driver = new FirefoxDriver(profile);
+	FirefoxProfile fxProfile = new FirefoxProfile();
 
 	    fxProfile.setPreference("browser.download.folderList",2);
 	    fxProfile.setPreference("browser.download.manager.showWhenStarting",false);
 	    fxProfile.setPreference("browser.download.dir","/Users/Dev/Downloads");
-	    fxProfile.setPreference("browser.helperApps.neverAsk.saveToDisk","text/csv");*/
+	    fxProfile.setPreference("browser.helperApps.neverAsk.saveToDisk","text/csv");
 
-	    WebDriver driver = new FirefoxDriver();
-
+	    WebDriver driver = new FirefoxDriver();*/
+		System.setProperty("phantomjs.binary.path", "/usr/local/bin/phantomjs");	
+		DesiredCapabilities cap = DesiredCapabilities.phantomjs();	
+		WebDriver driver = new PhantomJSDriver(cap);
+		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(10,TimeUnit.SECONDS);
-		driver.get("https://live.guru99.com/index.php/backendlogin");
+		driver.get("http://live.guru99.com/index.php/backendlogin");
+		Thread.sleep(10000);
+		 TakesScreenshot scrShot =((TakesScreenshot)driver);
+
+	        //Call getScreenshotAs method to create image file
+
+	                File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
+
+	            //Move image file to new destination
+
+	                File DestFile=new File("/Users/Dev/Downloads/screenshot.jpg");
+
+	                //Copy file at destination
+
+	                FileUtils.copyFile(SrcFile, DestFile);
+		//new WebDriverWait(driver, 30).until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='username']"))).sendKeys("user01");
 		driver.findElement(By.xpath("//*[@id='username']")).sendKeys("user01");
 		driver.findElement(By.xpath("//*[@id='login']")).sendKeys("guru99com");
 		driver.findElement(By.xpath("//*[@class='form-button']")).click();
